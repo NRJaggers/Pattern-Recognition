c560ed6bffb560d4facf94bc3537dca3fd376ef6
Nathan Jaggers <46210291+NRJaggers@users.noreply.github.com>
2023-04-25 17:57:41 -0700

    Progress!
    I def need to work on it

--- Project 3/Project_3.m
+++ Project 3/Project_3.m
@@ -12,12 +12,40 @@
 close all;
 clear;
 clc;
+%%
+%read in image
+im0 = imread("mnist_train0.jpg");
+im1 = imread("mnist_train1.jpg");
+im2 = imread("mnist_train2.jpg");
 
 %%
+%create binary image and show results
+binary_im0 = imbinarize(im0);
+%%imshow(binary_im0);
+binary_im1 = imbinarize(im1);
+%%imshow(binary_im1);
+binary_im2 = imbinarize(im2);
+%%imshow(binary_im2);
+
+%%
+features_0 = get_features(binary_im0);
+features_1 = get_features(binary_im1);
+features_2 = get_features(binary_im2);
+
+%%
+featarry_0 = make_feat_array(features_0);
+featarry_1 = make_feat_array(features_1);
+featarry_2 = make_feat_array(features_2);
+
+%% classifying 0 and 1
 %initialize variables for training
-%Y is augmented matrix holding 1's and two values to describe digit in each
-%row 
-Y = [1 1 1;1 0 1;1 1 0;1 1 1];
+%Y is augmented matrix holding (+/-)1's and two values to describe digit 
+%in each row 
+
+class_1 = [ones(length(featarry_0(:,2)),1) featarry_0(:,2) featarry_0(:,3)];
+class_2 = [-ones(length(featarry_0(:,2)),1) featarry_0(:,2) featarry_0(:,3)];
+Y = [class_1; class_2];
+
 
 %a is weight (column) vector to be adjusted
 %rows of a should equal the rows of Y
@@ -37,7 +65,7 @@ while (not_tuned)
         classification = y*a;
         if(classification <= 0)
             %update wights
-            a = a + y;
+            a = (a' + y)';
             %change exit status
             not_tuned = true;
         end
